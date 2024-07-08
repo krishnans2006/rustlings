67d16a3675a08429bbccae7c024a8ad27a24a30b
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-08 16:23:47 -0400

    Solve move_semantics4.rs

--- solutions/06_move_semantics/move_semantics4.rs
+++ solutions/06_move_semantics/move_semantics4.rs
@@ -1,4 +1,21 @@
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: Fix the compiler errors only by reordering the lines in the test.
+    // Don't add, change or remove any line.
+    #[test]
+    fn move_semantics5() {
+        let mut x = 100;
+        let y = &mut x;
+        // `y` used here.
+        *y += 100;
+        // The mutable reference `y` is not used anymore,
+        // therefore a new reference can be created.
+        let z = &mut x;
+        *z += 1000;
+        assert_eq!(x, 1200);
+    }
 }
