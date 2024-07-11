e1683f3b54138b1195a1b11b77b40719aeb6c89a
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-11 13:04:13 -0400

    Improve modules3.rs

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -3,8 +3,7 @@
 
 // TODO: Bring `SystemTime` and `UNIX_EPOCH` from the `std::time` module into
 // your scope. Bonus style points if you can do it with one line!
-use std::time::SystemTime;
-use std::time::UNIX_EPOCH;
+use std::time::{ SystemTime, UNIX_EPOCH };
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
