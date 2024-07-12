231918cc47b7468e2b27c55b63ef9a9686431636
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-12 15:22:45 -0400

    Solve hashmaps3.rs

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,16 @@ fn build_scores_table(results: &str) -> HashMap<&str, Team> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+
+        // Add details for team_1
+        let team_1_previous_goals = scores.entry(team_1_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        team_1_previous_goals.goals_scored += team_1_score;
+        team_1_previous_goals.goals_conceded += team_2_score;
+
+        // Add details for team_2
+        let team_2_previous_goals = scores.entry(team_2_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        team_2_previous_goals.goals_scored += team_2_score;
+        team_2_previous_goals.goals_conceded += team_1_score;
     }
 
     scores
